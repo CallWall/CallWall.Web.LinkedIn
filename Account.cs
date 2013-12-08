bb87a9620dcd9b95720302200b55a814ffe4f003
Lee Campbell <Lee.ryan.campbell@gmail.com>
2013-12-08 23:24:32 +0000

    Rebasing MultiSession with the resynch of the Templates

---
+++ Account.cs
@@ -0,0 +1,17 @@
+﻿namespace CallWall.Web.LinkedInProvider
+{
+    public sealed class Account : IAccount
+    {
+        private readonly string _userName;
+        private readonly string _displayName;
+
+        public Account(string userName, string displayName)
+        {
+            _userName = userName;
+            _displayName = displayName;
+        }
+
+        public string Username { get { return _userName; } }
+        public string DisplayName { get { return _displayName; } }
+    }
+}
